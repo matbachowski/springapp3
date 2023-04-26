7b445e7caaa07ec9c7137cb53690b96f73108970
unknown <matebac338@student.polsl.pl>
2023-04-26 11:15:42 +0200

    Initial commit

---
+++ src/main/java/pl/zajecia/springapp/SpringappApplication.java
@@ -0,0 +1,21 @@
+package pl.zajecia.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappApplication.class, args);
+	}
+
+	@GetMapping
+	public String get() {
+		return "Hello world";
+	}
+
+}
